3f1700959942d064791f92b8d6a99304a2900891
Jakub Janarek <jjanarek@gmail.com>
2019-05-31 22:29:27 +0200

    passport.js index.js router.js and controllers dir properly formatted according to eslint-airbnb and working in es6

--- bin/be/passport.js
+++ bin/be/passport.js
@@ -1,27 +1,36 @@
-/*made by David Aleixo @ KBZ david.aleixo@knowledgebiz.pt*/
+/* eslint-disable func-names */
+/* eslint-disable no-console */
+/* made by David Aleixo @ KBZ david.aleixo@knowledgebiz.pt */
 
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-var dal = require('./DAL');
+const dal = require("./DAL");
 
-passport.use(new LocalStrategy({
-    usernameField: 'username'
-  },
-  function(username, password, done) {
-    console.log("using passport...");
-    dal.accounts.getbyUserName(username, function(e, user){
-      if(e){
-        return done(e);
-      }
-      if(!user){
-        return done(null, false, {message: 'Account not found'})
-      }
-      if(!dal.accounts.validPassword(user, password)){
-        return done(null, false, {message: 'Password is wrong'})
-      }
-      //do not return the hash and salt
-      return done(null, {id: user.idaccounts, username: user.username, idroles: user.idroles});
-    })
-  }
-));
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username"
+    },
+    function(username, password, done) {
+      console.log("using passport...");
+      dal.accounts.getbyUserName(username, function(e, user) {
+        if (e) {
+          return done(e);
+        }
+        if (!user) {
+          return done(null, false, { message: "Account not found" });
+        }
+        if (!dal.accounts.validPassword(user, password)) {
+          return done(null, false, { message: "Password is wrong" });
+        }
+        // do not return the hash and salt
+        return done(null, {
+          id: user.idaccounts,
+          username: user.username,
+          idroles: user.idroles
+        });
+      });
+    }
+  )
+);
