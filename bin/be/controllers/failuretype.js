518a0025d889b9e78349b526f5981b88bd1cf3af
Jakub Janarek <jjanarek@gmail.com>
2019-06-08 13:22:42 +0200

    add failuretypes get and create

--- bin/be/controllers/failuretype.js
+++ bin/be/controllers/failuretype.js
@@ -1,22 +1,37 @@
-// const dal = require("../DAL");
+const failuretypes = require("./../DAL/failuretypes");
+const logger = require("./../logger");
 
 module.exports = {
   get(req, res) {
-    if (true) {
-      res.status(501).json({ message: "Not implemented" });
-    } else {
-      res.status(422).json({ message: "Missing required fields" });
-    }
+    failuretypes
+      .get()
+      .then(val => {
+        logger.debug(val);
+        res.status(200).send(val);
+      })
+      .catch(err => {
+        logger.error(err);
+        res.status(500).end();
+      });
   },
   create(req, res) {
-    if (true) {
-      res.status(501).json({ message: "Not implemented" });
+    if (req.query.name) {
+      failuretypes
+        .create({ name: req.query.name })
+        .then(val => {
+          logger.debug(val);
+          res.status(200).send(val);
+        })
+        .catch(err => {
+          logger.error(err);
+          res.status(500).end();
+        });
     } else {
-      res.status(422).json({ message: "Missing required fields" });
+      res.status(422).json({ message: "Missing required 'name' field" });
     }
   },
   update(req, res) {
-    if (true) {
+    if (req.query.id) {
       res.status(501).json({ message: "Not implemented" });
     } else {
       res.status(422).json({ message: "Missing required fields" });
